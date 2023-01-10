bd3c89511a1ed3b4efd873a7b071a9b26d2fc050
BuildTools <unconfigured@null.spigotmc.org>
2023-01-10 15:06:44 -0500

    added port forward for photonlib over usb

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -29,6 +30,9 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    PortForwarder.add(5800, "photonvision.local", 5800); //PORT FORWARD USB
+    PortForwarder.add(1181, "photonvision.local", 1181); //PORT FORWARD UsB
+    PortForwarder.add(1182, "photonvision.local", 1182); //PORT FORWARD UsB
   }
 
   /**
